eebe2dbef50115c6c741550ed68c3d9892a25c73
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-22 09:08:34 +0800

    updates

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -8,6 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Web | {{ $title ?? 'Not Set' }}</title>
+    <meta property="og:title" content="My Web | {{ $title ?? 'Not Set' }}">
+    <meta property="og:description" content="AJM E-Commerce Web App">
+    <meta property="og:image" content="favicon.ico">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="AJM E-Commerce Web App">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
         integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
