01a19be2e7cd6583e0796f45aaf8cb49a91448a3
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-03-25 16:58:10 +0800

    test

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,17 +24,13 @@ window.Echo = new Echo({
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
-    // wsHost: import.meta.env.VITE_PUSHER_HOST
-    //     ? import.meta.env.VITE_PUSHER_HOST
-    //     : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    // wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    // wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    wsHost: window.location.hostname,
-    wssHost: window.location.hostname,
-    wssPort: 6001,
-    wsPort: 6001,
-    // forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
-    forceTLS: false,
+    wsHost: import.meta.env.VITE_PUSHER_HOST
+        ? import.meta.env.VITE_PUSHER_HOST
+        : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    encrypted: true,
     disableStats: true,
     enabledTransports: ["ws", "wss"],
     authEndpoint: '/broadcasting/auth',
