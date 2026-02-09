04bdb073ad898e806fdd6e227c58a24d77cee3a3
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-02-09 08:35:16 +0800

    Home fixed

--- app/Livewire/NormalView/Pages/Home.php
+++ app/Livewire/NormalView/Pages/Home.php
@@ -5,9 +5,11 @@ namespace App\Livewire\NormalView\Pages;
 use App\Models\Cart;
 use App\Models\Favorite;
 use App\Models\Product;
-use App\Models\User;
+use App\Models\ProductColor;
+use App\Models\ProductSize;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Home extends Component
@@ -18,35 +20,111 @@ class Home extends Component
     public $morning;
     public $afternoon;
     public $evening;
+    public $has_sizes = false;
+    public $has_colors = false;
+
+    #[Validate('required_if:has_sizes,true', message: 'Please select a product size.')]
+    public $product_size_id = null;
+
+    #[Validate('required_if:has_colors,true', message: 'Please select a product color.')]
+    public $product_color_id = null;
+
+    public function toggleProductVariant($title, $id)
+    {
+        if ($title == 'size') {
+            if (ProductSize::find($id)->stock === 0) {
+                $this->product_size_id = null;
+                return $this->addError('product_size_id', 'Product with this size is out of stock.');
+            }
+            $this->product_size_id = $id === $this->product_size_id ? null : $id;
+            $this->resetErrorBag('product_size_id');
+        }
+
+        if ($title == 'color') {
+            if (ProductColor::find($id)->stock === 0) {
+                $this->product_color_id = null;
+                return $this->addError('product_color_id', 'Product with this color is out of stock.');
+            }
+            $this->product_color_id = $id === $this->product_color_id ? null : $id;
+            $this->resetErrorBag('product_color_id');
+        }
+    }
 
     public function essentialItems()
     {
-        $topDeals = Product::with(['product_category', 'favorites.product'])
-            ->where('product_sold', '>', 0)
-            ->orderBy('product_sold', 'desc')
+        $topDeals = Product::with(['product_category', 'productImages', 'favorites.product'])
+            ->withSum([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled")
+            ], 'order_quantity')
+            ->withCount([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled"),
+                'productRatings'
+            ])
+            ->whereHas(
+                'orders',
+
+                fn($query)
+                =>
+                $query->where('order_status', '!=', "Cancelled")
+            )
+            ->orderBy('orders_sum_order_quantity', 'desc')
             ->take(value: 12)
             ->get();
 
-        $popularityDeals = Product::with(['product_category', 'favorites.product'])
-            ->where('product_votes', '>', 0)
-            ->orderBy('product_votes', 'desc')
+        $popularityDeals = Product::with(['product_category', 'productImages', 'favorites.product'])
+            ->withSum([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled")
+            ], 'order_quantity')
+            ->withCount([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled"),
+                'productRatings'
+            ])
+            ->has('productRatings')
+            ->orderBy('product_ratings_count', 'desc')
             ->take(12)
             ->get();
 
-        $latestProducts = Product::with(['product_category', 'favorites.product'])->orderBy('created_at', 'desc')
+        $latestProducts = Product::with(['product_category', 'productImages', 'favorites.product'])
+            ->withSum([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled")
+            ], 'order_quantity')
+            ->withCount([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled"),
+                'productRatings'
+            ])
+            ->orderBy('id', 'desc')
             ->take(12)
             ->get();
 
-        $allLocations = User::all();
-
-        return compact('topDeals', 'popularityDeals', 'latestProducts', 'allLocations');
+        return compact('topDeals', 'popularityDeals', 'latestProducts');
     }
 
     public function addToCartNowItem($id)
     {
+        $this->validate();
 
         $cart = Cart::where('user_id', auth()->id())
             ->where('product_id', $id)
+            ->where('product_size_id', $this->product_size_id)
+            ->where('product_color_id', $this->product_color_id)
             ->first();
 
         if ($cart) {
@@ -55,14 +133,18 @@ class Home extends Component
             ]);
         } else {
             Cart::create([
-                'user_id' => auth()->id(),
-                'product_id' => $id,
-                'quantity' => 1,
+                'user_id'          => auth()->id(),
+                'product_id'       => $id,
+                'quantity'         => 1,
+                'product_size_id'  => $this->product_size_id,
+                'product_color_id' => $this->product_color_id
             ]);
         }
 
         $this->dispatch('toastr', data: ['type' => 'success', 'message' => 'Product added to cart successfully.']);
         $this->dispatch('closeModal');
+        $this->reset();
+
         return;
     }
 
@@ -89,7 +171,31 @@ class Home extends Component
 
     public function view($id)
     {
-        $this->productView = Product::find($id);
+        $this->reset();
+
+        $this->productView = Product::withCount([
+            'productRatings',
+            'orders'
+            =>
+            fn($query)
+            => $query->where('order_status', '!=', "Cancelled")
+        ])
+            ->withSum([
+                'orders'
+                =>
+                fn($query)
+                => $query->where('order_status', '!=', "Cancelled")
+            ], 'order_quantity')
+            ->with([
+                'product_category',
+                'productSizes',
+                'productColors',
+                'productImages'
+            ])
+            ->find($id);
+
+        $this->has_sizes = $this->productView->productSizes->isNotEmpty();
+        $this->has_colors = $this->productView->productColors->isNotEmpty();
     }
 
     #[On('closedModal')]
