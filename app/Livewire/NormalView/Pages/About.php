aa76e7514ee6107e9e7e948c9558476c67159534
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-03-13 14:17:22 +0800

    functions added and datas

--- app/Livewire/NormalView/Pages/About.php
+++ app/Livewire/NormalView/Pages/About.php
@@ -3,6 +3,9 @@
 namespace App\Livewire\NormalView\Pages;
 
 use App\Models\Contact;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductRating;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -16,7 +19,24 @@ class About extends Component
             ->where('is_published', true)
             ->get(['name', 'email', 'message', 'created_at']);
 
-        return compact('testimonies');
+        $product_available = Product::query()
+            ->where('product_status', "Available")
+            ->count();
+
+        $happy_customers = ProductRating::query()
+            ->distinct('user_id')
+            ->count();
+
+        $orders_delivered = Order::query()
+            ->where(
+                fn($query)
+                =>
+                $query->where('order_status', 'Delivered')
+                    ->orWhere('order_status', 'Paid')
+            )
+            ->count();
+
+        return compact('testimonies', 'product_available', 'happy_customers', 'orders_delivered');
     }
 
     public function render()
